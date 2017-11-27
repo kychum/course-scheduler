6fcd3b90b7e9f397a40a2a3dc1edc56daf6af826
blowe574 <blowe574@gmail.com>
2017-11-27 13:12:42 -0700

    Minor Changes to constraints; added test inputs

--- src/common/Constraints.java
+++ src/common/Constraints.java
@@ -1,16 +1,45 @@
 package common;
 import java.util.TreeMap;
 
+// Initialized once; the constraints class object is meant to store all the various mappings that define the problem constraints.
 public class Constraints {
-  TreeMap<Assignable, Assignable> pair; // Represents case where two courses cannot both be in same timeslot
+  TreeMap<Assignable, Assignable> pair; // Represents case where two courses should be paired together
+  TreeMap<Assignable, Assignable> incomp; // Represents case where two courses cannot both be in same timeslot
   TreeMap<Assignable, Slot> timeslot; // represents case where a course cannot be in a certain timeslot
+  // penalty values for various penalties
+  private int pen_coursemin; // timeslot not filled to minimal courses
+  private int pen_labsmin; // timeslot not filled to minimal labs
+  private int pen_notpaired; // courses that aren't paired together
+  private int pen_section; // sections mapped to the same section
   public Constraints() {
 	  // initialize
 	  pair = new TreeMap<>();
+	  incomp = new TreeMap<>();
+	  timeslot = new TreeMap<>();
   }
-  public boolean addConstraint(Assignable a1, Assignable a2) {
+  public boolean addIncomp(Assignable a1, Assignable a2) {
 	  // Returns true if adding constraint succeeded, false if failed
-	  if (checkConstraint(a1, a2)) {
+	  if (checkIncomp(a1, a2)) {
+		  return false;
+	  }
+	  else {
+		  incomp.put(a1, a2);
+		  return true;
+	  }
+  }
+  
+  public boolean checkIncomp(Assignable a1, Assignable a2) {
+	  if (incomp.get(a1).equals(a2) || incomp.get(a2).equals(a1))
+	  {
+		  return true;
+	  }
+	  else {
+		  return false;
+	  }
+  }
+  
+  public boolean addPair(Assignable a1, Assignable a2) {
+	  if (checkPair(a1, a2)) {
 		  return false;
 	  }
 	  else {
@@ -19,7 +48,7 @@ public class Constraints {
 	  }
   }
   
-  public boolean checkConstraint(Assignable a1, Assignable a2) {
+  public boolean checkPair(Assignable a1, Assignable a2) {
 	  if (pair.get(a1).equals(a2) || pair.get(a2).equals(a1))
 	  {
 		  return true;
@@ -28,4 +57,6 @@ public class Constraints {
 		  return false;
 	  }
   }
+  
+  
 }
