kychum/course-scheduler

commit 897177e62f9f3b97279ab2dde2701d9a36d2583c

Fix crash when adding partAssign for 813/913.

src/common/Assignment.java
@@ -37,6 +37,7 @@ public class Assignment {
   public Assignment(Instance instance) {
     this.instance = instance;
     assignments = new TreeMap<Slot, HashSet<Assignable>>();
+    assignments.put( Slot.getSpecialSlot(), new HashSet<>() );
     for( Slot s : instance.getCourseSlots() ) {
       assignments.put( s, new HashSet<Assignable>() );
     }

commit f5e370639e591e254fd2206ca36b5fbde83a017c

Fix test failures caused by 897177e6

src/common/Assignment.java
@@ -37,7 +37,6 @@ public class Assignment {
   public Assignment(Instance instance) {
     this.instance = instance;
     assignments = new TreeMap<Slot, HashSet<Assignable>>();
-    assignments.put( Slot.getSpecialSlot(), new HashSet<>() );
     for( Slot s : instance.getCourseSlots() ) {
       assignments.put( s, new HashSet<Assignable>() );
     }
@@ -46,6 +45,9 @@ public class Assignment {
     }
     courseAssignments = new TreeMap<Assignable, Slot>();
 
+    if( instance.getPartAssign().keySet().stream().filter( assn -> assn.getCourseNum() == 813 || assn.getCourseNum() == 913 ).count() > 0 ) {
+      assignments.put( Slot.getSpecialSlot(), new HashSet<>() );
+    }
     instance.getPartAssign().forEach( (assign, slot) -> {
       add( slot, assign );
     });